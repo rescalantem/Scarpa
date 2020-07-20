82c96294f72bbfed79af2005de2ee2ca1e98a8eb
ricardo alejandro escalante mercado <ricescal@hotmail.com>
2020-07-20 13:14:08 -0500

    Common y Web

---
+++ Scarpa.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Scarpa.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ Scarpa.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Scarpa.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
